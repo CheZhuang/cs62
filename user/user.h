07380f3a25537e5ce22197ccf149edef7754a605
Tan <tan@lomia.uk>
2025-04-08 01:26:03 +0000

    222

--- user/user.h
+++ user/user.h
@@ -2,7 +2,22 @@
 #define USER_H
 
 class User {
-    public: User();
+    private:
+    int id_;
+    std::string name_;
+    int year_;
+    int zip_;
+    std::set<int> friends;
+
+    public:
+    User();
+    void addFriend(int id);
+    void deleteFriend(int id);
+    int getId() { return id_; }
+    std::string getName() { return name_; }
+    int getYear() { return year_; }
+    int getZip() { return zip_; }
+    std::set<int> getfriends { return friends; }
 };
 
 #endif
